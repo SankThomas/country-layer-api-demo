6d71733991381ca95fd8bb82c87f5b33dcc1ad9b
Thomas Sankara <thomassankara74@gmail.com>
2021-09-30 10:31:53 +0300

    set up react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Countries from "./components/Countries"
+import Country from "./components/Country"
+import Header from "./components/Header"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          {/* Filter components */}
+          <Countries />
+        </Route>
+        <Route path="/:capital" children={<Country />}></Route>
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
